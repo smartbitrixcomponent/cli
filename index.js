4710c01ad89a5d68dedb101376f6eb648b527e11
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2017-11-07 23:25:47 +0200

    Добавляет метод обновления ядра

--- index.js
+++ index.js
@@ -1,47 +1,71 @@
 #!/usr/bin/env node
 const prompt = require('commander');
 const fs = require('fs');
-const sh = require("shelljs");
+const sh = require('shelljs');
+
 const lockfile = 'smatrix.lock';
-const github = require('github-download')
+const github = require('github-download');
 
-prompt.version('0.1.0');
-    // .option('create <namespace:component> [template]', ' Create component')
+prompt.version('0.0.4');
+prompt.option('-a, --all', 'Update all core files and gulp config. Use with UPDATE');
 
-let cwd = sh.pwd();
-const __DIR__ = cwd.stdout;
-const lockfilePath = __DIR__ + '/' + lockfile;
+const cwd = sh.pwd();
+const baseDir = cwd.stdout;
+const lockfilePath = `${baseDir}/${lockfile}`;
 
 prompt.command('init')
-.description('init project')
-.action(() => {
+  .description('init project')
+  .action(() => {
     console.log('start...');
-    fs.exists(lockfilePath, exists => {
-        if(exists) {
-            console.log('Your project allready initialized');
-        } else {
-            // Забираем ядро php из пакета и создаем файл smatrix.lock для проекта
-            github({user: 'smartbitrixcomponent', repo: 'smatrix-core', ref: 'master'}, __DIR__ + "/tmp")
-            .on('error', function(err) {
-                console.error(err)
-            })
-            .on('end', () => {
-                console.log('Success!');
-                sh.cp('-r', __DIR__ + '/tmp/*', __DIR__);
-                sh.cp('-r', __DIR__ + '/tmp/.*', __DIR__);
-                sh.rm('-rf', __DIR__ + '/tmp');
-            });
-            fs.writeFile(lockfilePath, "", err => {if(err) console.log('Error');});
-
-        }
+    fs.exists(lockfilePath, (exists) => {
+      if (exists) {
+        console.log('Your project allready initialized');
+      } else {
+        // Забираем ядро php из пакета и создаем файл smatrix.lock для проекта
+        github({ user: 'smartbitrixcomponent', repo: 'smatrix-core', ref: 'master' }, `${baseDir}/tmp`)
+          .on('error', (err) => {
+            console.error(err);
+          })
+          .on('end', () => {
+            console.log('Success!');
+            sh.cp('-r', `${baseDir}/tmp/*`, baseDir);
+            sh.cp('-r', `${baseDir}/tmp/.*`, baseDir);
+            sh.rm('-rf', `${baseDir}/tmp`);
+          });
+        fs.writeFile(lockfilePath, '', (err) => {
+          if (err) console.log('Error');
+        });
+      }
     });
-});
+  });
 
+prompt.command('update')
+  .description('Update core')
+  .action(() => {
+    console.log('💻   Update...');
+    github({ user: 'smartbitrixcomponent', repo: 'smatrix-core', ref: 'master' }, `${baseDir}/tmp`)
+      .on('error', (err) => {
+        console.error(err);
+      })
+      .on('end', () => {
+        console.log('Success!');
+        sh.cp('-r', `${baseDir}/tmp/local/core/`, `${baseDir}/local/`);
+        sh.cp('-r', `${baseDir}/tmp/local/helpers/`, `${baseDir}/local/`);
+        if (prompt.all) {
+          sh.cp('-r', `${baseDir}/tmp/gulpfile.js`, baseDir);
+          sh.cp('-r', `${baseDir}/tmp/package.json`, baseDir);
+        }
+        sh.rm('-rf', `${baseDir}/tmp`);
+      });
+    fs.writeFile(lockfilePath, '', (err) => {
+      if (err) console.log('Error');
+    });
+  });
 
 prompt.command('create [namespace:component] [template]')
-.description('create component template')
-.action((cmd, option) => {
+  .description('create component template')
+  .action((cmd, option) => {
     console.log(cmd, option);
-});
+  });
 
 prompt.parse(process.argv);
